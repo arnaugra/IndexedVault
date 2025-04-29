89da5846536a9698b01a63685ac56959538dd7a7
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-04-29 11:31:22 +0200

    feat EncryptStore: implement Zustand store for encryption key management

---
+++ src/stores/EncryptStore.ts
@@ -0,0 +1,44 @@
+import { create } from 'zustand';
+
+interface EncryptStore {
+    openModal: boolean;
+    setOpenModal: (b: boolean) => void;
+
+    encryptionKey: string | null;
+    setEncryptionKey: (key: string) => void;
+    saveEncryptionKey: () => void;
+    loadEncryptionKey: () => void;
+    deleteEncryptionKey: () => void;
+}
+
+const useEncryptStore = create<EncryptStore>((set) => ({
+    openModal: false,
+    setOpenModal: (b) => set({ openModal: b }),
+    encryptionKey: null,
+    setEncryptionKey: (key) => set({ encryptionKey: key }),
+    saveEncryptionKey: () => {
+        const key = useEncryptStore.getState().encryptionKey;
+        if (key) {
+            sessionStorage.setItem("encryptionKey", key);
+        }
+    },
+    loadEncryptionKey: () => {
+        const key = sessionStorage.getItem("encryptionKey");
+        if (key) {
+            set({ encryptionKey: key });
+        } else {
+            set({ encryptionKey: null });
+        }
+    },
+    deleteEncryptionKey: () => {
+        set({ encryptionKey: null });
+        sessionStorage.removeItem("encryptionKey");
+    }
+}));
+
+function getEncryptionKey () {
+    return sessionStorage.getItem("encryptionKey") ?? null;
+}
+
+export default useEncryptStore;
+export { getEncryptionKey };
