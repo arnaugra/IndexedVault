f30c76271c52b244b54cef9164d7e210e121b173
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-05-02 21:32:31 +0200

    refactor ValueStore: simplify store

--- src/stores/ValueStore.ts
+++ src/stores/ValueStore.ts
@@ -5,18 +5,6 @@ interface ValueStore {
     setOpenNewValue: (open: boolean) => void;
     valueIdToEdit?: number;
     setValueIdToEdit: (id: number | undefined) => void;
-    valueName?: string;
-    valueNameError: boolean;
-    valueValue?: string | boolean;
-    valueValueError: boolean;
-    valueType: ValueType;
-    valueExpirationDate?: string;
-    setValueName: (name: string | undefined) => void;
-    setValueNameError: (error: boolean) => void;
-    setValueValue: (value: string | boolean | undefined) => void;
-    setValueValueError: (error: boolean) => void;
-    setValueType: (type: ValueType) => void;
-    setValueExpirationDate: (date: string | undefined) => void;
 }
 
 enum ValueTypes {
@@ -25,25 +13,11 @@ enum ValueTypes {
     DATE = "DATE",
 }
 
-type ValueType = keyof typeof ValueTypes;
-
 const useValueStore = create<ValueStore>((set) => ({
     openNewValue: false,
     setOpenNewValue: (open) => set({ openNewValue: open }),
     valueIdToEdit: undefined,
     setValueIdToEdit: (id) => set({ valueIdToEdit: id }),
-    valueName: undefined,
-    valueNameError: false,
-    valueValue: undefined,
-    valueValueError: false,
-    valueType: "TEXT",
-    valueExpirationDate: undefined,
-    setValueName: (name) => set({ valueName: name, valueNameError: false }),
-    setValueNameError: (error) => set({ valueNameError: error }),
-    setValueValue: (value) => set({ valueValue: value, valueValueError: false }),
-    setValueValueError: (error) => set({ valueValueError: error }),
-    setValueType: (type) => set({ valueType: type }),
-    setValueExpirationDate: (date) => set({ valueExpirationDate: date })
 }));
 
 export default useValueStore;
