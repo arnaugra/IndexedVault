cd8084db3801c786535c10e2b76d89105155a0b8
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-05-26 10:55:28 +0200

    refactor Config: enhance error handling in methods

--- src/db/Config.ts
+++ src/db/Config.ts
@@ -1,6 +1,10 @@
+import useErrorStore, { ErrorsTypes, genericError } from "../stores/ErrorStore";
+import { createError } from "../utils/error";
 import { db } from "./db";
 import { ConfigI } from "./interfaces";
 import { Model } from "./Model";
+
+const { addError } = useErrorStore.getState()
 
 export class Config extends Model<ConfigI, "id"> {
     constructor() {
@@ -8,26 +12,69 @@ export class Config extends Model<ConfigI, "id"> {
     }
 
     static async create(value: Omit<ConfigI, "id">) {
-        const existingConfig = await db.config.where("name").equals(value.name).first();
+        try {
+            const existingConfig = await db.config.where("name").equals(value.name).first();
 
-        if (existingConfig && existingConfig.id) {
-            return await db.config.update(existingConfig.id, value)
-        } else {
-            return await db.config.add(value);
+            if (existingConfig && existingConfig.id) {
+                return await db.config.update(existingConfig.id, value)
+            } else {
+                return await db.config.add(value);
+            }
+
+        } catch (error) {
+            genericError("creating the configuration");
+            throw error;
+
         }
-
     }
 
     static async getByName(name: string) {
-        return db.config.where("name").equals(name).first();
-    }
+        try {
+            const conf = await db.config.where("name").equals(name).first();
+            if (!conf) throw new ConfigGetError(`Configuration with name ${name} not found`);
+            return conf;
 
-    static async update(id: number, updates: Partial<ConfigI>) {
-        await db.config.update(id, updates);
-        return db.config.get(id);
+        } catch (error) {
+            ConfigGetError.errorIsInstanceOf(error, (error) => {
+                addError({
+                    id: Math.random(),
+                    message: error.message,
+                    type: ErrorsTypes.error,
+                    timestamp: Date.now()
+                });
+                throw error;
+            })
+
+            genericError("fetching the configuration");
+            throw error;
+
+        }
     }
 
     static async delete(id: number) {
-        return db.config.delete(id);
+        try {
+            const existingConfig = await db.config.get(id);
+            if (!existingConfig) throw new ConfigDeleteError(`Configuration with id ${id} not found`);
+            return db.config.delete(id);
+
+        } catch (error) {
+            ConfigDeleteError.errorIsInstanceOf(error, (error) => {
+                addError({
+                    id: Math.random(),
+                    message: error.message,
+                    type: ErrorsTypes.error,
+                    timestamp: Date.now()
+                });
+                throw error;
+            });
+
+            genericError("deleting the configuration");
+            throw error;
+            
+        }
+        
     }
 }
+
+const ConfigGetError = createError("ConfigGetError");
+const ConfigDeleteError = createError("ConfigDeleteError");
