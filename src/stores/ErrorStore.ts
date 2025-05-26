674068fa4d2e9720766d4ef769180f2141d076d5
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-05-26 10:45:10 +0200

    feat(ErrorStore): add genericError function for standardized error handling

--- src/stores/ErrorStore.ts
+++ src/stores/ErrorStore.ts
@@ -27,6 +27,21 @@ const useErrorStore = create<ErrorStore>((set) => ({
     removeError: (id) => set((state) => ({ errors: state.errors.filter((error) => error.id !== id) })),
 }));
 
+/**
+ * Generates a generic error message for unexpected operations.
+ * This function is used to create a standardized error message when an unexpected error occurs during operations such as saving data, loading configuration, etc.
+ * @param message - A description of the operation that caused the error, e.g., "saving data", "loading configuration".
+ * @example "An unexpected error occurred while [message]"
+ */
+const genericError = (message: string) => {
+    useErrorStore.getState().addError({
+        id: Math.random(),
+        message: `An unexpected error occurred while ${message}`,
+        type: ErrorsTypes.error,
+        timestamp: Date.now(),
+    });
+}
+
 export default useErrorStore;
 export type { ErrorsType };
-export { ErrorsTypes };
+export { ErrorsTypes, genericError };
