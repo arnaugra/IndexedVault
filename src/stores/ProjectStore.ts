a53a456fb04ea87c432d51c545b01f857adff1cc
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-10-02 00:54:54 +0200

    feat (ProjectStore, SectionStore): add UUID support

--- src/stores/ProjectStore.ts
+++ src/stores/ProjectStore.ts
@@ -1,17 +1,22 @@
 import { create } from 'zustand';
+import { UUID } from '../types/fields';
 
 interface ProjectStore {
     projectName?: string;
     projectDescription?: string;
+    projectUuid?: UUID;
     setProjectName: (name: string | undefined) => void;
     setProjectDescription: (description: string | undefined) => void;
+    setProjectUuid: (uuid: UUID | undefined) => void;
 }
 
 const useProjectStore = create<ProjectStore>((set) => ({
     projectName: undefined,
     projectDescription: undefined,
+    projectUuid: undefined,
     setProjectName: (name) => set({ projectName: name }),
     setProjectDescription: (description) => set({ projectDescription: description }),
+    setProjectUuid: (uuid) => set({ projectUuid: uuid }),
 }));
 
 export default useProjectStore;
--- src/stores/SectionStore.ts
+++ src/stores/SectionStore.ts
@@ -1,17 +1,22 @@
 import { create } from 'zustand';
+import { UUID } from '../types/fields';
 
 interface SectionStore {
     sectionName?: string;
     sectionDescription?: string;
+    sectionUuid?: UUID;
     setSectionName: (name: string | undefined) => void;
     setSectionDescription: (description: string | undefined) => void;
+    setSectionUuid: (uuid: UUID | undefined) => void;
 }
 
 const useSectionStore = create<SectionStore>((set) => ({
     sectionName: undefined,
     sectionDescription: undefined,
+    sectionUuid: undefined,
     setSectionName: (name) => set({ sectionName: name }),
     setSectionDescription: (description) => set({ sectionDescription: description }),
+    setSectionUuid: (uuid) => set({ sectionUuid: uuid }),
 }));
 
 export default useSectionStore;
