3f150fcfe72ca9586edd8f2b6f54cb7ba4e48515
Arnau Granados Liébana <arnau.gra@gmail.com>
2025-10-15 01:40:24 +0200

    refactor BreadcrumbsComponent: replace store hooks with context hooks for project and section data

--- src/components/BreadcrumbsComponent.tsx
+++ src/components/BreadcrumbsComponent.tsx
@@ -1,28 +1,28 @@
-import useProjectStore from "../stores/ProjectStore";
-import useSectionStore from "../stores/SectionStore";
 import FolderIcon from "../svg/FolderIcon";
 import FileIcon from "../svg/FileIcon";
 import { Link } from "wouter";
 import BreadcrumbSeparatorIcon from "../svg/BreadcrumbSeparatorIcon";
+import { useProject } from "../contexts/ProjectContext";
+import { useSection } from "../contexts/SectionContext";
 
 function BreadcrumbsComponent() {
 
-    const { projectName, projectUuid } = useProjectStore();
-    const { sectionName, sectionUuid } = useSectionStore();
+    const { currentProject } = useProject();
+    const { currentSection } = useSection();
 
   return (
     <div className="flex justify-start items-center gap-1 w-full">
 
-        <Link href={`/project/${projectUuid}`} className={(active) => `${active ? "active-link-breadcrumb" : ""} ${sectionName ? "max-w-[25%]" : ""} flex items-center gap-1`}>
-            <FolderIcon className={`${sectionName ? "w-4" : "w-5"} mt-0.5 shrink-0`} />
-            <p className={`line-clamp-1 @max-xl/layout:hidden ${sectionName ? "text-sm" : "text-lg"}`}>{projectName}</p>
+        <Link href={`/project/${currentProject?.uuid}`} className={(active) => `${active ? "active-link-breadcrumb" : ""} ${currentSection ? "max-w-[25%]" : ""} flex items-center gap-1`}>
+            <FolderIcon className={`${currentSection ? "w-4" : "w-5"} mt-0.5 shrink-0`} />
+            <p className={`line-clamp-1 @max-xl/layout:hidden ${currentSection ? "text-sm" : "text-lg"}`}>{currentProject?.name}</p>
         </Link>
 
-        {sectionUuid && <>
+        {currentSection && <>
             <BreadcrumbSeparatorIcon className="active-link-breadcrumb w-5" />
-            <Link href={`/project/${projectUuid}/section/${sectionUuid}`} className={(active) => `${active ? "active-link-breadcrumb" : ""} flex items-center gap-1`}>
+            <Link href={`/project/${currentProject?.uuid}/section/${currentSection?.uuid}`} className={(active) => `${active ? "active-link-breadcrumb" : ""} flex items-center gap-1`}>
                 <FileIcon className="w-5 shrink-0" />
-                <p className="line-clamp-1 text-lg">{sectionName}</p>
+                <p className="line-clamp-1 text-lg">{currentSection?.name}</p>
             </Link>
         </>}
     </div>
